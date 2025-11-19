a0342bf4beb8057172d0d7a1f68cf7ffa42ee127
Immortal_pi <36298498+Immortal-Pi@users.noreply.github.com>
2025-11-18 20:19:53 -0600

    Update Cognito configuration in config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_pO0llYvTX', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3f2h3jmjlj86gb2jbe9orqn3p8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
